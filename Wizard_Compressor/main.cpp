14ee3050ec0c4160cc318da9df2324eb1cbe3488
orann11 <clippingsipping@gmail.com>
2026-01-10 23:45:50 +0300

    Setup changes

--- Wizard_Compressor/main.cpp
+++ Wizard_Compressor/main.cpp
@@ -10,9 +10,11 @@ int main()
     std::ifstream file("./input.txt");
     std::string line;
 
+
     while(std::getline(file, line)){
         std::cout << line << '\n';
     }
+    print_line(123);
 
     return 0;
 }
