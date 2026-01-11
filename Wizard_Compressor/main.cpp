ff3a551ed76e9e449fb6fcb01baba6d6803a561e
orann11 <clippingsipping@gmail.com>
2026-01-11 23:12:04 +0300

    binary conversion added for visualization

--- Wizard_Compressor/main.cpp
+++ Wizard_Compressor/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <bitset>
+#include <vector>
+
+std::string convert_to_binary(std::string line){
+    std::string binary_version = "";
+    for(auto i = 0; i < line.size(); i++){
+        char letter = line.at(i);
+        std::string bin_letter = std::bitset<8>(letter).to_string();
+
+        binary_version = bin_letter + "_"+ binary_version;
+    }
+    binary_version += "00000000";
+    return binary_version;
+}
 
 
 int main()
@@ -14,7 +28,10 @@ int main()
     while(std::getline(file, line)){
         std::cout << line << '\n';
     }
-    print_line(123);
-
+    std::string new_line = "oran";
+    std::vector<std::string> v;
+    std::string binary_version = convert_to_binary(new_line);
+    std::cout << binary_version << '\n';
     return 0;
 }
+
