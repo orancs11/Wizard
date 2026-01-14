37757c59a439326d589e6b2bb6a48d123cf474a0
orann11 <clippingsipping@gmail.com>
2026-01-15 01:07:20 +0300

    Halfly implemented PriorityQueue

--- Wizard_Compressor/PriorityQueue.hpp
+++ Wizard_Compressor/PriorityQueue.hpp
@@ -4,23 +4,54 @@
 
 template <typename T, typename U>
 struct Pair {
-    T _first;
-    U _second;
-    Pair(T first, U second): first{_first}, second{_second}
-    {}
+    T first;
+    U second;
+
 };
 
 template <typename T, typename U>
 class PriorityQueue {
     private:
-        std::vector<Pair<T, U>> q;
+        std::vector<Pair<T, U>> _q;
 
     public:
-        virtual void swim(int i);
-        virtual void sink(int i);
-        virtual void add(const Pair<T, U>& freq_pair);
-        virtual void vanish(int index);
+         PriorityQueue(){}
+        ~PriorityQueue(){}
+
+        void add(const Pair<T, U> &p)
+        {
+            this._q.push_back(p);
+            swim();
+
+        }
+        Pair<T, U> pop();
+
+        void swim()
+        {
+            int last_index = _q.size() - 1;
+            int curr_index{last_index};
+            bool condition{true};
+
+
+            while(condition){
+                int parent = (curr_index - 1) / 2;
+                if(this._q.at(parent).second < this._q.at(curr_index).second)
+                {
+                    std::swap(_q.at(parent), _q.at(curr_index));
+                }
+                else condition = false;
+                curr_index = parent;
+            }
+        }
+
+        void sink();
 };
+
+
+
+
+
+
 
 
 #endif
